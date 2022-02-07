f3b41740ece29913e79b753b461a63b3165d62dd
David L Norris <dave@webaugur.com>
2022-02-07 07:55:24 -0500

    New test to make sure Zoom executable exists and is executable Ask for Meeting ID via Kodi Numeric dialog.

--- addon.py
+++ addon.py
@@ -2,8 +2,21 @@ import os
 import xbmcaddon
 import xbmcgui
  
+# Zoom URL Format
+#    zoommtg://zoom.us/join?action=join&confno=<MEETING ID>
+zoom_url         = "zoommtg://zoom.us/join\?action=join\&confno="
+zoom_exec        = "/usr/bin/zoom"
 addon       = xbmcaddon.Addon()
 
-# execute zoom.us app natively
-os.system("/usr/bin/zoom");
+# If Zoom is executable then ask for Meeting ID
+if os.access(zoom_exec, os.X_OK):
+    # Meeting ID Input Dialog.
+    #   Kodi should scrub the input as long as we are set to Numeric...
+    zoom_meetid = xbmcgui.Dialog().input("Zoom.US Meeting ID", "", xbmcgui.INPUT_NUMERIC)
+    # execute zoom.us app natively on Linux
+    os.system(zoom_exec + " " + zoom_url + zoom_meetid)
+
+# If zoom is not executable then ask the user to install Zoom
+else: 
+    xbmcgui.Dialog().ok("Zoom Error", "Please install Zoom from https://zoom.us to /usr/bin/zoom")
 
